73f9898e5aa7800af4d71483c52d0e828dd2a464
王咖喱 <2722933638@qq.com>
2023-08-30 00:00:16 +0800

    feat:初始化网关

---
+++ gali-api-gateway/src/test/java/com/gali/gateway/GaliApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gali.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaliApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
