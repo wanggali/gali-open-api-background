38b9a3ea7adb310df4f560d8391e5a021d565650
王咖喱 <2722933638@qq.com>
2023-09-05 23:48:23 +0800

    feat:整合dubbo

--- gali-api-gateway/src/main/java/com/gali/gateway/GaliApiGatewayApplication.java
+++ gali-api-gateway/src/main/java/com/gali/gateway/GaliApiGatewayApplication.java
@@ -1,13 +1,15 @@
 package com.gali.gateway;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class GaliApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GaliApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GaliApiGatewayApplication.class, args);
+    }
 
 }
